1bc7780e576cb375c7220bff525c991ef3dfd3a6
Сергей <sburcev100@gmail.com>
2025-11-05 17:01:08 +0500

    Исправил баг с точками в Embla Carousel

---
+++ src/js/components/embla.js
@@ -0,0 +1,20 @@
+import EmblaCarousel from "embla-carousel"
+
+export function emblaCarousel() {
+    return {
+        embla: null,
+        dots: [],
+        selected: 0,
+        init() {
+            this.embla = EmblaCarousel(this.$refs.viewport, { loop: true })
+            this.dots = Array.from({ length: this.embla.slideNodes().length })
+            this.embla.on('select', () => this.updateDots())
+
+            this.updateDots()
+        },
+
+        updateDots() {
+            this.selected = this.embla.selectedScrollSnap()
+        }
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,8 @@
 import '../css/main.css';
 import Alpine from 'alpinejs';
-import EmblaCarousel from 'embla-carousel';
+import { emblaCarousel } from "./components/embla"
 
-// === Alpine ===
-document.addEventListener('alpine:init', () => {
-    Alpine.store('price', {
-        min: 39,
-        max: 300,
-        minValue: 39,
-        maxValue: 300,
-        inRange(price) {
-            return price >= this.minValue && price <= this.maxValue;
-        },
-    });
-});
 
+
+window.emblaCarousel = emblaCarousel
 Alpine.start();
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const emblaNode = document.querySelector('.embla');
-    const viewportNode = emblaNode.querySelector('.embla__viewport');
-    const embla = EmblaCarousel(viewportNode, {
-        loop: true,
-        align: 'start',
-        skipSnaps: false,
-    });
-
-
-
-
-    // Точки
-    const dots = emblaNode.querySelector('.embla__dots');
-    const slidesCount = embla.slideNodes().length;
-
-    for (let i = 0; i < slidesCount; i++) {
-        const dot = document.createElement('button');
-        dot.className = 'embla__dot w-3 h-3 rounded-full bg-gray-300';
-        dot.addEventListener('click', () => embla.scrollTo(i));
-        dots.appendChild(dot);
-    }
-
-    const updateDots = () => {
-        const selected = embla.selectedScrollSnap();
-        dots.querySelectorAll('.embla__dot').forEach((dot, index) => {
-            dot.classList.toggle('bg-green-500', index === selected);
-        });
-    };
-
-    embla.on('select', updateDots);
-    updateDots();
-});
