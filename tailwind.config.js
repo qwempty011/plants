b2c903c994502ecb68b6ac15f788b746ceed2a8e
Сергей <sburcev100@gmail.com>
2025-12-08 22:59:28 +0500

    правки

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,10 @@
 export default {
     content: ["./src/**/*.{js,ts,jsx,tsx,html,twig}"],
     theme: {
-
         extend: {
             fontFamily: {
-                montserrat: ['Montserrat', 'sans-serif'],
-            },
-            colors: {
-                darkgray: '#3D3D3D',
+                roboto: ['Roboto', 'sans-serif'],
             },
         },
-    }
+    },
 };
